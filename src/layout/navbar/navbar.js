c053de0d02d6732836a1d1def39a3224b2ed28ae
samdev <samstore637@gmail.com>
2023-07-04 17:54:47 +0300

    started to see the navbar

--- src/layout/navbar/navbar.js
+++ src/layout/navbar/navbar.js
@@ -1,167 +1,105 @@
-import React, { useState, useLayoutEffect, useRef } from 'react';
-import { NavLink } from 'react-router-dom';
+import PropTypes from "prop-types";
+import { useRef } from "react";
+// @mui
+import { useTheme } from "@mui/material/styles";
+import { Box, Button, AppBar, Toolbar, Container, Link } from "@mui/material";
+// hooks
+import useOffSetTop from "../../hooks/useOffSetTop";
+import useResponsive from "../../hooks/useResponsive";
+// utils
+import { bgBlur } from "../../utils/cssStyles";
+// config
+import { HEADER } from "../../config-global";
+// routes
+// import { PATH_DOCS, PATH_MINIMAL_ON_STORE } from "../../routes/paths";
+// components
+import Logo from "../../components/logo";
+import Label from "../../components/label";
+//
+import {navConfig} from "./info";
+import NavMobile from "./mobile";
+import NavDesktop from "./desktop";
 
-import { styled } from '@mui/system';
-import { AppBar,Toolbar, IconButton, Typography, Button, Menu, MenuItem, Divider, Container, Stack, Box, Paper } from '@mui/material';
+// ----------------------------------------------------------------------
 
-import { FaBars } from "react-icons/fa";
+export default function Navbar() {
+	const carouselRef = useRef(null);
 
-
-import { menuItems, navConfig } from "./info";
-
-import SwipeableSideDrawer from './drawer';
-import useResponsive from 'src/hooks/useResponsive';
-import NavDesktop from './desktop/NavDesktop';
-import useOffSetTop from 'src/hooks/useOffSetTop';
-import { HEADER } from 'src/config';
-import NavMobile from './mobile/NavMobile';
-
-const logo = "https://res.cloudinary.com/dbj0t0zym/image/upload/v1687960483/logos/logowhite_qirfnd.png"
-
-const TopAppBar = styled(AppBar)(({ theme }) => ({
-	zIndex: 3,
-	backgroundColor: theme.palette.primary.main,
-}))
-
-const StyledContainer = styled(Container)({
-	width: "100%",
-	paddingTop: "5px",
-	paddingBottom: "5px",
-})
-
-const StyledLogoSection = styled(Box)({
-	borderRadius: "50%",
-	padding: "0px"
-})
-
-const StyledMenuStack = styled(Stack)({
-	width: "100%",
-	paddintTop: "20px",
-	paddintBottom: "20px",
-});
-
-const StyledNavButton = styled(Button) ({
-	paddingTop: "20px",
-	paddingBottom: "20px",
-	width:"max-content"
-});
-
-const styledLink = {
-	textDecoration: "none",
-	color: "inherit"
-}
-
-const StyledButtonStack = styled(Stack)({
-	
-})
-
-const StyledButton = styled(Button)(({ theme }) => ({
-	minWidth: "200px",
-	backgroundColor: "#ffffff",
-	color: theme.palette.primary.main 
-}))
-
-const styledLogo = {
-	height: "60px"
-}
-
-const styledMobileLogo = {
-	height: "40px"
-}
-
-const iconButtonSX = {
-
-}
-
-const Navigation = () => {
-
-	const [mobileNav, setMobileNav] = useState(false);
+	const theme = useTheme();
 
 	const isDesktop = useResponsive("up", "md");
 
 	const isOffset = useOffSetTop(HEADER.H_MAIN_DESKTOP);
 
-	const handleMobileNav = () => {
-		setMobileNav(!mobileNav)
-	}
-
-	const innerWidth = window.innerWidth
-
 	return (
-		<>
-			{/* {
-				innerWidth >= 1000 ? (
-					<TopAppBar position="scroll">
-						<Toolbar>
-							<StyledContainer maxWidth="xl">
-
-								<StyledMenuStack direction="row" justifyContent="space-between" alignItems="center" spacing={5}>
-									<StyledLogoSection>
-										<img src={logo} alt="Skydive logo" style={styledLogo}/>
-									</StyledLogoSection>
-
-									<Stack direction="row" spacing={3}>
-										{
-											menuItems.map((el, i) => (
-												<StyledNavButton sx={{textAlign: "left"}} variant="text" key={i}>
-													<NavLink to={el.path} style={styledLink}>
-														<Typography variant="subtitle1" style={{textTransform: "uppercase", color: "#fff"}}>
-															{el.label}
-														</Typography>
-													</NavLink>
-
-												</StyledNavButton>
-											))
-										}
-									</Stack>
+		<AppBar ref={carouselRef} color="transparent" sx={{ boxShadow: 0 }}>
+			<Toolbar
+				disableGutters
+				sx={{
+					height: {
+						xs: HEADER.H_MOBILE,
+						md: HEADER.H_MAIN_DESKTOP,
+					},
+					transition: theme.transitions.create(["height", "background-color"], {
+						easing: theme.transitions.easing.easeInOut,
+						duration: theme.transitions.duration.shorter,
+					}),
+					...(isOffset && {
+						...bgBlur({ color: theme.palette.background.default }),
+						height: {
+							md: HEADER.H_MAIN_DESKTOP - 16,
+						},
+					}),
+				}}
+			>
+				<Container maxWidth="xl" sx={{ height: 1, display: "flex", alignItems: "center" }}>
+					<Logo />
 
 
-									<StyledButtonStack direction="row" spacing={3}>
-										<StyledButton variant="contained" color="secondary">
-											<Typography variant="subtitle1" >
-												Request a quote
-											</Typography>
-										</StyledButton>
-									</StyledButtonStack>
-								</StyledMenuStack>
-							</StyledContainer>
+					<Box sx={{ flexGrow: 1 }} />
 
-						</Toolbar>
-					</TopAppBar>
-				) : (
-					<Box sx={{marginBottom: "52px"}}>
-						<TopAppBar >
-							<Container maxWidth="xl">
-								<Stack direction="row" justifyContent="space-between" alignItems="center">
-									<StyledLogoSection>
-										<img src={logo} alt="Henriot mobile logo" style={styledMobileLogo}/>
-									</StyledLogoSection>
+					{isDesktop && <NavDesktop isOffset={isOffset} data={navConfig} />}
 
-									<IconButton
-										size="large"
-										aria-label="account of current user"
-										aria-controls="menu-appbar"
-										aria-haspopup="true"
-										onClick={handleMobileNav}
-										color="inherit"
-										sx={iconButtonSX}
-									>
-										<FaBars/>
-									</IconButton>
-								</Stack>
-							</Container>
-						</TopAppBar>
-					</Box>
-				)
+					<Button
+						variant="contained"
+						target="_blank"
+						rel="noopener"
+					>
+						Purchase Now
+					</Button>
 
-			} */}
-			{isDesktop && <NavDesktop isOffset={isOffset} data={navConfig} />}
-			
-			{!isDesktop && <NavMobile isOffset={isOffset} data={navConfig} />}
-			
-			{/* <SwipeableSideDrawer mobileNav={mobileNav} setMobileNav={setMobileNav} /> */}
-		</>
+					{!isDesktop && <NavMobile isOffset={isOffset} data={navConfig} />}
+				</Container>
+			</Toolbar>
+
+			{isOffset && <Shadow />}
+		</AppBar>
 	);
+}
+
+// ----------------------------------------------------------------------
+
+Shadow.propTypes = {
+	sx: PropTypes.object,
 };
 
-export default Navigation;
+function Shadow({ sx, ...other }) {
+	return (
+		<Box
+			sx={{
+				left: 0,
+				right: 0,
+				bottom: 0,
+				height: 24,
+				zIndex: -1,
+				m: "auto",
+				borderRadius: "50%",
+				position: "absolute",
+				width: `calc(100% - 48px)`,
+				boxShadow: (theme) => theme.customShadows.z8,
+				...sx,
+			}}
+			{...other}
+		/>
+	);
+}
