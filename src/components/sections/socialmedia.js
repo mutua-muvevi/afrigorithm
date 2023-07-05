8d996a537011e5debde97a4276143743659d2be8
samdev <samstore637@gmail.com>
2023-07-06 00:24:23 +0300

    added social media icons

---
+++ src/components/sections/socialmedia.js
@@ -0,0 +1,22 @@
+import Iconify from "../iconify/iconify"
+
+const SocialMediaIcons = [
+	{
+		icon: <Iconify icon="ic:outline-facebook"/>,
+		link: ""
+	},
+	{
+		icon: <Iconify icon="bi:twitter"/>,
+		link: "https://twitter.com/afrigorithm"
+	},
+	{
+		icon: <Iconify icon="ri:instagram-fill"/>,
+		link: ""
+	},
+	{
+		icon: <Iconify icon="mdi:linkedin"/>,
+		link: ""
+	},
+]
+
+export default SocialMediaIcons
