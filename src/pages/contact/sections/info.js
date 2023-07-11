b834f0c3d30b5f98abcf3cdbc632cd55feb749b3
samdev <samstore637@gmail.com>
2023-07-12 02:25:38 +0300

    added more cards in contact

--- src/pages/contact/sections/info.js
+++ src/pages/contact/sections/info.js
@@ -18,3 +18,66 @@ export const contactItems = [
 		text: "Visit us in Nairobi - witness the harmonious blend of tech and ambition."
 	},
 ]
+
+export const helpList = [
+	{
+		icon: "fluent:document-text-20-filled",
+		title: "Documentation",
+		mainText:
+			"Elit at imperdiet dui accumsan sit. Sem nulla pharetra diam sit amet nisl.",
+		items: [
+			{
+				icon: "fa-solid:file-pdf",
+				text: "Download PDF Documents",
+			},
+			{
+				icon: "fa-solid:file-pdf",
+				text: "Download PDF Documents",
+			},
+			{
+				icon: "fa-solid:file-pdf",
+				text: "Download PDF Documents",
+			},
+		],
+	},
+	{
+		icon: "clarity:flow-chart-solid",
+		title: "Work flow",
+		mainText:
+			"Elit at imperdiet dui accumsan sit. Sem nulla pharetra diam sit amet nisl.",
+		items: [
+			{
+				icon: "fa-solid:file-pdf",
+				text: "Download PDF Documents",
+			},
+			{
+				icon: "fa-solid:file-pdf",
+				text: "Download PDF Documents",
+			},
+			{
+				icon: "fa-solid:file-pdf",
+				text: "Download PDF Documents",
+			},
+		],
+	},
+	{
+		icon: "ri:admin-fill",
+		title: "For Clients",
+		mainText:
+			"Elit at imperdiet dui accumsan sit. Sem nulla pharetra diam sit amet nisl.",
+		items: [
+			{
+				icon: "fa-solid:file-pdf",
+				text: "Download PDF Documents",
+			},
+			{
+				icon: "fa-solid:file-pdf",
+				text: "Download PDF Documents",
+			},
+			{
+				icon: "fa-solid:file-pdf",
+				text: "Download PDF Documents",
+			},
+		],
+	},
+];
