36d2a7fdd711b5f59482e28b7779b2da12d486f4
samdev <samstore637@gmail.com>
2023-07-12 00:28:06 +0300

    added banner

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,7 +1,12 @@
 import { Box } from "@mui/material";
 import { styled } from "@mui/system";
+import ReusableBanner from "src/components/sections/banner";
+import ContactForm from "src/components/sections/contactform";
 
 import Page from "src/components/ui/Page";
+
+const image ="https://res.cloudinary.com/dbj0t0zym/image/upload/v1689110485/contactus_veng3s.jpg";
+const subtitle="Ignite your journey towards unparalleled digital growth by getting in touch with us today. Your conquest of the future starts with this vital connection - let's chart your course together."
 
 const StyledWrapper = styled(Box)(({ theme }) => ({
 
@@ -11,7 +16,14 @@ const Contact = () => {
 	return (
 		<Page title="Contact us">
 			<StyledWrapper>
-				<div>Contact</div>
+				<ReusableBanner
+					title="Contact us"
+					subtitle={subtitle}
+					image={image}
+					height="60vh"
+					alignItems="center"
+				/>
+				<ContactForm/>
 			</StyledWrapper>
 		</Page>
 	)
---
+++ src/pages/contact/sections/cards.js
@@ -0,0 +1,18 @@
+import { Box, Container } from "@mui/material";
+import { styled } from "@mui/system";
+
+const StyledWrapper = styled(Box)(({ theme }) => ({
+	
+}))
+
+const StyledContainer= styled(Container)(({ theme }) => ({
+
+}))
+
+const ContactCards = () => {
+	return (
+		<div>ContactCards</div>
+	)
+}
+
+export default ContactCards
