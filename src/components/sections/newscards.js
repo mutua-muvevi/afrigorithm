7c9b62770129e0735be9dd2eedddae0c41bbf397
samdev <samstore637@gmail.com>
2023-07-13 15:25:41 +0300

    worked on reducers, types and actions for news, research and blogs

--- src/components/sections/newscards.js
+++ src/components/sections/newscards.js
@@ -3,6 +3,8 @@ import { styled } from "@mui/system";
 
 import TitleSubtitle from "./titlesubtitle";
 import { truncateStr } from "src/utils/formatStrings";
+import { selectNews } from "src/redux/news/action";
+import { connect } from "react-redux";
 
 const StyledWrapper = styled(Container)(({ theme }) => ({
 	paddingTop: "100px",
@@ -14,11 +16,16 @@ const StyledCard = styled(Card)(({ theme }) => ({
 	minHeight: 450
 }))
 
-const StyledCardContent = styled(CardActionArea)(({ theme }) => ({
+const StyledCardActionArea = styled(CardActionArea)(({ theme }) => ({
 	height: "100%"
 }))
 
-const ReusableNewsCards = ({title, subtitle, alignItems, chipText, list}) => {
+const ReusableNewsCards = ({title, subtitle, alignItems, chipText, list, setNews}) => {
+
+	const handleSelectNews = (news) => {
+		setNews(news)
+	}
+
 	return (
 		<StyledWrapper maxWidth="xl">
 			<Stack direction="column" spacing={5} justifyContent="center">
@@ -31,27 +38,27 @@ const ReusableNewsCards = ({title, subtitle, alignItems, chipText, list}) => {
 				<div>
 					<Grid container spacing={3}>
 						{
-							list.map((el, i) => (
+							list.map((news, i) => (
 								<Grid item xs={12} sm={12} md={6} lg={4} xl={3} key={i}>
 									<StyledCard raised>
-										<StyledCardContent>
+										<StyledCardActionArea onClick={() => handleSelectNews(news)}>
 											<CardMedia
 												component="img"
-												src={el.thumbnail}
-												alt={`${el.title} : Afrigorithm Blog thumbnail`}
+												src={news.thumbnail}
+												alt={`${news.title} : Afrigorithm Blog thumbnail`}
 												height={250}
 											/>
 											<CardContent>
 												<Stack direction="column" spacing={3}>
 													<Typography variant="h6">
-														{ truncateStr(el.title, 55) }
+														{ truncateStr(news.title, 55) }
 													</Typography>
 													<Typography variant="body1" textAlign="justify">
-														{truncateStr(el.mainParagraphs[0], 100)}
+														{truncateStr(news.mainParagraphs[0], 100)}
 													</Typography>
 												</Stack>
 											</CardContent>
-										</StyledCardContent>
+										</StyledCardActionArea>
 									</StyledCard>
 								</Grid>
 							))
@@ -63,4 +70,10 @@ const ReusableNewsCards = ({title, subtitle, alignItems, chipText, list}) => {
 	)
 }
 
-export default ReusableNewsCards
+const mapStateToProps = ({}) => ({});
+
+const mapDispatchToProps = (dispatch) => ({
+	setNews : (news) => dispatch(selectNews(news))
+}); 
+
+export default connect(mapStateToProps, mapDispatchToProps)(ReusableNewsCards)
---
+++ src/redux/news/reducer.js
@@ -0,0 +1,37 @@
+import newsTypes from './types';
+
+const initialState = {
+	data: null,
+	isLoading: false,
+
+	news:null,
+	newsError: null,
+};
+
+const newsReducer = (state = initialState, { type, payload }) => {
+	switch (type) {
+		case newsTypes.START_SELECT_NEWS:
+			return { 
+				...state,
+				loading: true,
+			};
+		case newsTypes.SUCCESS_SELECT_NEWS:
+			return {
+				...state,
+				loading: false,
+				news: payload
+			};
+		case newsTypes.FAIL_SELECT_NEWS:
+			return {
+				...state,
+				loading: false,
+				news: null,
+				newsError: payload,
+			};
+
+		default:
+			return state;
+	}
+};
+
+export default newsReducer;
