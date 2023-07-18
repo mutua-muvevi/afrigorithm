771f7a40a91fb0eea0304a6e38a5e05890243d87
samdev <samstore637@gmail.com>
2023-07-18 20:04:45 +0300

    added little bit content in the paragraph

--- src/pages/about/sections/founder.js
+++ src/pages/about/sections/founder.js
@@ -4,7 +4,7 @@ import { styled } from "@mui/system";
 import TitleSubtitle from "src/components/sections/titlesubtitle";
 import { founders } from "./info";
 
-const subtitle = "Meet our Visionaries, the Driving Force Behind our Tech Revolution. With a shared passion for innovation and a relentless pursuit of excellence, our founders are redefining the possibilities of technology.";
+const subtitle = "Meet our Visionaries, the Driving Force Behind our Tech Revolution. With a shared passion for innovation and a relentless pursuit of excellence, our founders are redefining the possibilities of technology. Embracing the uncharted realms of technological advancements, our founders bring more than just expertise. Their vision transcends traditional boundaries, weaving a digital tapestry where every thread signifies growth and resilience. Their leadership, fostering a culture of innovation, positions our clients at the forefront of their industries, championing a future where technology isn't just a tool, but the catalyst of their triumph";
 
 const StyledWrapper = styled(Box)(({ theme }) => ({
 
